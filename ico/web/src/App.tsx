5e49b70a6067776bdfddf04f4d9bdf5a43437338
JonasMS <jonas.sota@gmail.com>
2021-11-18 15:22:14 -0700

    display num tokens, WIP

--- ico/web/src/App.tsx
+++ ico/web/src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ethers } from "ethers"
 import Container from '@mui/material/Container';
 import Box from "@mui/material/Box";
+import SotanoCoinJSON from "../../artifacts/contracts/SotanoCoin.sol/SotanoCoin.json";
 
-function App() {
+const SOTANOCOIN_ICO_CONTRACT = "0x093568B8e4812f1D252e0FD6573c745aB8B765Ca";
+
+declare global {
+  interface Window { ethereum: any; }
+}
+
+const App = () => {
+  const [numTokens, setNumTokens] = useState(0);
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(SOTANOCOIN_ICO_CONTRACT, SotanoCoinJSON.abi, provider);
+
+
+
   return (
     <Container maxWidth="lg">
-      <Box>Test</Box>
+      <Box>{`Tokens: ${numTokens}`}</Box>
+      {/* Display how many tokens a user owns */}
     </Container>
   );
 }
