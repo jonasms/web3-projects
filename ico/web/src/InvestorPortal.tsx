7c2ef6dcce64df1964d70dbcbee98bb6958cf94e
JonasMS <jonas.sota@gmail.com>
2021-11-18 19:33:03 -0700

    purchase tokens from FE

--- ico/web/src/InvestorPortal.tsx
+++ ico/web/src/InvestorPortal.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import { ethers } from "ethers"
 import Container from '@mui/material/Container';
 import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import SotanoCoinJSON from "../../artifacts/contracts/SotanoCoin.sol/SotanoCoin.json";
 
 const SOTANOCOIN_ICO_CONTRACT = process.env.REACT_APP_CONTRACT_ADDRESS;
@@ -13,26 +16,85 @@ declare global {
 }
 
 const { utils } = ethers;
-const { formatEther } = utils;
+const { parseEther, formatEther } = utils;
 
-const InvestorPortalView = ({ account, contract }: any) => {
-  const [numTokens, setNumTokens] = useState(0);
-  const _account = useRef(account);
-  const _contract = useRef(contract);
+const InvestorPortalView = ({ account, ...props }: any) => {
+    const [statusMessage, setStatusMessage] = useState("");
+    const [numTokens, setNumTokens] = useState(0);
+    const [tokensToPurchase, setTokensToPurchase] = useState("0");
+//   const [curPhase, setCurPase] = useState();
+//   const account = useRef(props.account);
+    const contract = useRef(props.contract);
   
-  useEffect(() => {
-    _contract.current.investorToTokensOwed(_account.current).then((tokensOwed: any) => {
-        console.log("TOKENS OWED: ", formatEther(tokensOwed));
-        setNumTokens(parseFloat(formatEther(tokensOwed)));
-    });
-  },[_account, _contract])
+    useEffect(() => {
+        contract.current.investorToTokensOwed(account).then((tokensOwed: any) => {
+            console.log("TOKENS OWED: ", formatEther(tokensOwed));
+            setNumTokens(parseFloat(formatEther(tokensOwed)));
+        });
 
+    // TODO CUR PHASE
+    // contract.current.curPhase().then((phase: any) => {
+    //     setCurPhase(phase);
+    // });
+    },[account, contract])
 
+    // const handlePurchase = () => {
+    async function handlePurchase() {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const _contract = new ethers.Contract(
+            SOTANOCOIN_ICO_CONTRACT!,
+            SotanoCoinJSON.abi,
+            signer
+        );
+
+        console.log("PURCHASING TOKENS: ", parseEther(tokensToPurchase));
+
+        try {
+            const tsx = await _contract.purchase({ value: parseEther(tokensToPurchase) });
+            await tsx.wait();
+            setStatusMessage("Transaction Completed");
+        } catch (e) {
+            console.error(e);
+            setStatusMessage("Transaction Failed");
+        }
+    }
+    //     contract.current.purchase({ value: parseEther(tokensToPurchase) })
+    //         .then(() => {
+    //             setStatusMessage("Transaction Completed");
+    //         })
+    //         .catch(() => {
+    //             setStatusMessage("Transaction Failed");
+    //         });
+    // };
+
+  const enablePurchasing = tokensToPurchase && parseFloat(tokensToPurchase) > 0;
 
   return (
     <Container maxWidth="lg">
         <Box>{`Account: ${account}`}</Box>
-      <Box>{`Tokens: ${numTokens}`}</Box>
+        <Box mt={1}>{`Tokens: ${numTokens}`}</Box>
+      {/* <Box>{`Current Phase: ${curPhase}`}</Box> */}
+        <Box mt={4}>
+            <Box>
+                <TextField
+                    value={tokensToPurchase}
+                    onChange={(evt: any) => {
+                        console.log("EVENT: ", evt.target.value);
+                        setTokensToPurchase(evt.target.value);
+                    }}
+                    label="Number"
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </Box>
+            <Box mt={1}>
+                <Button variant="contained" disabled={!enablePurchasing} onClick={handlePurchase}>Purchase Tokens</Button>
+            </Box>
+            <Box mt={1}><Typography>{statusMessage}</Typography></Box>
+        </Box>
       </Container>
     )
 }
