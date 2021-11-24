c4281c5bc6487adfa961598a4c45c112850cb88f
JonasMS <jonas.sota@gmail.com>
2021-11-23 20:09:00 -0700

    dao: misc

--- dao/contracts/CollectorDAO.sol
+++ dao/contracts/CollectorDAO.sol
@@ -12,7 +12,7 @@ contract CollectorDAO is CollectorBase {
         keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
 
     /// @notice The EIP-712 typehash for the ballot struct used by the contract
-    bytes32 public constant BALLOT_TYPEHASH = keccak256("Ballot(uint256 proposalId,bool support)");
+    bytes32 public constant BALLOT_TYPEHASH = keccak256("Ballot(uint256 proposalId,uint8 support)");
 
     string public constant name = "Collector DAO Governor";
 
