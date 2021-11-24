0ba6270ca5e78d9e553d4ccefb2fada0a548f8a3
JonasMS <jonas.sota@gmail.com>
2021-11-24 16:27:39 -0700

    deploy test contracts

--- dao/contracts/Proposal.sol
+++ dao/contracts/Proposal.sol
@@ -1,10 +1,12 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-interface NftMarketplace {
+interface NftMarketplaceInt {
     function getPrice(address nftContract, uint256 nftId) external returns (uint256 price);
 
     function buy(address nftContract, uint256 nftId) external payable returns (bool success);
+
+    function addNftContract(address nftContract_, uint256[2][] calldata nfts_) external;
 }
 
 contract Proposal {
@@ -15,10 +17,10 @@ contract Proposal {
     }
 
     function buyNft(address nftContract, uint256 nftId) external payable {
-        uint256 price = NftMarketplace(marketplaceAddr).getPrice(nftContract, nftId);
+        uint256 price = NftMarketplaceInt(marketplaceAddr).getPrice(nftContract, nftId);
 
         if (price >= msg.value) {
-            bool success = NftMarketplace(marketplaceAddr).buy{ value: price }(nftContract, nftId);
+            bool success = NftMarketplaceInt(marketplaceAddr).buy{ value: price }(nftContract, nftId);
             require(success, "buyNft: nft purchase failed");
         }
     }
--- dao/tasks/deploy/CollectorDAO.ts
+++ dao/tasks/deploy/CollectorDAO.ts
@@ -1,11 +1,31 @@
+import { ethers } from "hardhat";
 import { task } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 
 import { CollectorDAO } from "../../src/types/CollectorDAO";
+import { NftMarketplace } from "../../src/types/NftMarketplace";
+import { Proposal } from "../../src/types/Proposal";
 
-task("deploy:SotanoCoin").setAction(async function (taskArguments: TaskArguments, { ethers }) {
+task("deploy:CollectorDAO").setAction(async function (taskArguments: TaskArguments, { ethers }) {
+  // deploy CollectorDAO
   const daoFactory = await ethers.getContractFactory("CollectorDAO");
-  const dao: CollectorDAO = <CollectorDAO>await daoFactory.deploy(process.env.REACT_APP_DEV_WALLET_ADDRESS);
+  const dao: CollectorDAO = <CollectorDAO>await daoFactory.deploy();
   await dao.deployed();
   console.log("CollectorDAO deployed to: ", dao.address);
+});
+
+task("deploy:SupportContracts").setAction(async function (taskArguments: TaskArguments, { ethers }) {
+  // deploy NftMarketplace
+  const nftMarketplaceFactory = await ethers.getContractFactory("NftMarketplace");
+  const marketplace: NftMarketplace = <NftMarketplace>await nftMarketplaceFactory.deploy();
+  await marketplace.deployed();
+  console.log("NftMarketplace deployed to: ", marketplace.address);
+
+  await marketplace.addNftContract("0x90F79bf6EB2c4f870365E785982E1f101E93b906", [[0, ethers.utils.parseEther("1")]]);
+
+  // deploy Proposal
+  const proposalFactory = await ethers.getContractFactory("Proposal");
+  const proposal: Proposal = <Proposal>await proposalFactory.deploy(marketplace.address);
+  await proposal.deployed();
+  console.log("Proposal deployed to: ", proposal.address);
 });
