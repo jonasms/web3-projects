bf93626a82df2823201d2a675c3e86ba3f441cec
JonasMS <jonas.sota@gmail.com>
2021-12-02 10:51:32 -0700

    impl BananaswapV1 _mint() and _burn()

--- lp/contracts/BananaswapV1.sol
+++ lp/contracts/BananaswapV1.sol
@@ -2,7 +2,22 @@
 pragma solidity >=0.8.4;
 
 contract BananaswapV1 {
-    // _mint()
-    // _burn()
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    function _mint(address to, uint256 value) internal {
+        balanceOf[to] += value;
+        totalSupply += value;
+    }
+
+    function _burn(address from, uint256 value) internal {
+        require(from != address(0), "Bananswap._burn: CANNOT BURN FROM ADDRESS 0");
+
+        uint256 amount = balanceOf[from];
+        require(amount >= value, "Bananswap._burn: INSUFFICIENT FUNDS");
+
+        balanceOf[from] -= value;
+        totalSupply -= value;
+    }
     // transfer(), transferFrom()?
 }
